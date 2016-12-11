dff5da91078170ede036eb56218ff5c0cc0f6313
barthdamon <mbmattbarth@gmail.com>
2016-12-11 12:48:57 -0800

    reinitializes repo with git ignore and c++ base

---
+++ Source/ludum/ludumGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ludumGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUM_API AludumGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
